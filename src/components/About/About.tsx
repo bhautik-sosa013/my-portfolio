9b566a8fb162aa42eb3a33575aabcd67cb0347c3
projectcode.dev <noreply@projectcode.dev>
2026-05-19 16:36:28 +0530

    Update from projectcode.dev

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,4 @@
-import ME from "../../assets/dainy_jose.jpg";
+import ME from "../../assets/batman.jpg";
 import { FaAward } from "react-icons/fa";
 import { TbMessageChatbot } from "react-icons/tb";
 import { MdOutlineMilitaryTech } from "react-icons/md";
