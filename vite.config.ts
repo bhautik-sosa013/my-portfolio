4e5cd25723cce9a95aacf7dd5bb03a9e9ed0379e
projectcode.dev <noreply@projectcode.dev>
2026-05-19 11:52:40 +0530

    Update from projectcode.dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 export default defineConfig({
+  server: { host: '::', port: 8080 },
   plugins: [react()],
   base: isGithubPages ? "/my-portfolio/" : "/",
 });
